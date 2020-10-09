15de9fc83a77ca585879a4f5f15dce90953e6fad
Guix98 <guimega.gh@gmail.com>
2020-10-08 22:47:34 -0400

    redundancia corregida

--- src/app/components/new-password/new-password.component.ts
+++ src/app/components/new-password/new-password.component.ts
@@ -9,11 +9,18 @@ export class NewPasswordComponent  {
   alerta: boolean;
 
   constructor() {
-    this.alerta=false;
+    this.alerta = false;
    }
-  cont(password1:string , password2:string){
+  cont(password1: string , password2: string){
     if (password1 != password2) {
-      this.alerta=true;
+      this.alerta = true;
+      console.log(this.alerta);
+      
+    }
+    else{
+      this.alerta = false;
+      console.log(this.alerta);
+      
     }
   }
 
